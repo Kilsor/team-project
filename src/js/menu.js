ca80e4718cb75ea3fd3a73290310ca5f9a511738
Kilsor <kilsors@gmail.com>
2023-04-24 08:49:08 +0300

    fix menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -20,4 +20,13 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+
+  const menuLinks = document.querySelectorAll('.menu-link');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.menu.classList.add('is-hidden');
+      document.body.classList.remove('no-scroll');
+    });
+  });
 })();
